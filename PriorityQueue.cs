48b3226f4af7592334a9d3ab714ba6454530a9e6
WilliamMcRoberts <wamcroberts2@gmail.com>
2023-12-19 10:00:27 -0500

    first commit

---
+++ PriorityQueue.cs
@@ -0,0 +1,94 @@
+class Person(string name, int age) : IComparable<Person>
+{
+    public string Name { get; set; } = name;
+    public int Age { get; set; } = age;
+
+    public int CompareTo(Person? other)
+    {
+        if (this.Age < other?.Age) return -1;
+        if (this.Age == other?.Age) return 0;
+        return 1;
+    }
+}
+
+class PriorityQueue<T>(bool isMaxHeap = true) where T : IComparable<T>
+{
+    private readonly List<T> _heap = [];
+
+    public int Count => _heap.Count;
+
+    public void Enqueue(T item)
+    {
+        _heap.Add(item);
+        BubbleUp(_heap.Count - 1);
+    }
+
+    public T Dequeue()
+    {
+        T item = _heap[0];
+        int lastIndex = _heap.Count - 1;
+        _heap[0] = _heap[lastIndex];
+        _heap.RemoveAt(lastIndex);
+        BubbleDown(0);
+        return item;
+    }
+
+    public T Peek() => _heap[0];
+
+    private void BubbleUp(int index)
+    {
+        while (index > 0)
+        {
+            int parentIndex = (index - 1) / 2;
+            if (isMaxHeap)
+            {
+                if (_heap[parentIndex].CompareTo(_heap[index]) >= 0)
+                    break;
+            }
+            else
+            {
+                if (_heap[parentIndex].CompareTo(_heap[index]) <= 0)
+                    break;
+            }
+            Swap(parentIndex, index);
+            index = parentIndex;
+        }
+    }
+
+    private void BubbleDown(int index)
+    {
+        while (index < _heap.Count)
+        {
+            int leftChildIndex = index * 2 + 1;
+            int rightChildIndex = index * 2 + 2;
+            if (!isMaxHeap)
+            {
+                int smallestchildIndex = index;
+                if (leftChildIndex < _heap.Count && _heap[leftChildIndex].CompareTo(_heap[smallestchildIndex]) < 0)
+                    smallestchildIndex = leftChildIndex;
+                if (rightChildIndex < _heap.Count && _heap[rightChildIndex].CompareTo(_heap[smallestchildIndex]) < 0)
+                    smallestchildIndex = rightChildIndex;
+                if (smallestchildIndex == index)
+                    break;
+
+                Swap(smallestchildIndex, index);
+                index = smallestchildIndex;
+                return;
+            }
+
+            int largestChildIndex = index;
+            if (leftChildIndex < _heap.Count && _heap[leftChildIndex].CompareTo(_heap[largestChildIndex]) > 0)
+                largestChildIndex = leftChildIndex;
+            if (rightChildIndex < _heap.Count && _heap[rightChildIndex].CompareTo(_heap[largestChildIndex]) > 0)
+                largestChildIndex = rightChildIndex;
+            if (largestChildIndex == index)
+                break;
+
+            Swap(largestChildIndex, index);
+            index = largestChildIndex;
+        }
+    }
+
+    private void Swap(int i, int j) =>
+        (_heap[j], _heap[i]) = (_heap[i], _heap[j]);
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿var maxHeap = new PriorityQueue<int>();
+
+maxHeap.Enqueue(5);
+maxHeap.Enqueue(4);
+maxHeap.Enqueue(3);
+maxHeap.Enqueue(2);
+maxHeap.Enqueue(9);
+maxHeap.Enqueue(8);
+maxHeap.Enqueue(7);
+maxHeap.Enqueue(6);
+maxHeap.Enqueue(1);
+
+int peakElement = maxHeap.Peek();
+
+// Print the peak element
+Console.WriteLine("Max Peak element: " + peakElement);
+
+var minHeap = new PriorityQueue<int>(false);
+
+minHeap.Enqueue(5);
+minHeap.Enqueue(4);
+minHeap.Enqueue(3);
+minHeap.Enqueue(20);
+minHeap.Enqueue(380);
+
+Console.WriteLine("Min Peak element: " + minHeap.Peek());
+
+var p1 = new Person("John", 30);
+var p2 = new Person("Jane", 25);
+var p3 = new Person("Jack", 40);
+var p4 = new Person("Jill", 35);
+
+var maxHeapPerson = new PriorityQueue<Person>();
+
+maxHeapPerson.Enqueue(p1);
+maxHeapPerson.Enqueue(p2);
+maxHeapPerson.Enqueue(p3);
+maxHeapPerson.Enqueue(p4);
+
+Person maxAgePerson = maxHeapPerson.Peek();
+
+Console.WriteLine("Max age person: " + maxAgePerson.Name);
+
+
+var minHeapPerson = new PriorityQueue<Person>(false);
+
+minHeapPerson.Enqueue(p1);
+minHeapPerson.Enqueue(p2);
+minHeapPerson.Enqueue(p3);
+minHeapPerson.Enqueue(p4);
+
+Person minAgePerson = minHeapPerson.Peek();
+
+Console.WriteLine("Min age person: " + minAgePerson.Name);
